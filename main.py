008c8bffd002b64042f4dd74718def73d43c237e
A <andr25@email.ua>
2022-10-13 15:23:31 +0300

    login module update

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_json import FlaskJSON, JsonError, json_response
 
 from login_module import load_login_module
 
-
 app = Flask(__name__)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/planex.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'test-secret-key'
 
 db = SQLAlchemy(app)
-
 
 if __name__ == "__main__":
     load_login_module(app, db)
