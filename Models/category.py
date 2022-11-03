84754a40e5f465b725047cf1cbafeb4d17214c20
A <andr25@email.ua>
2022-11-03 10:29:43 +0200

    Completed Category CRUD

--- Models/category.py
+++ Models/category.py
@@ -1,6 +1,9 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+
 db = SQLAlchemy()
 
 
@@ -8,18 +11,30 @@ class CategoryModel(db.Model, UserMixin):
     __tablename__ = "Category"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
-    description = db.Column(db.TEXT)
+    description = db.Column(db.TEXT, nullable=True)
     # -----------FK !-------------------
-    # user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, name, description, user_id):
-        self.name = name
-        self.description = description
-        self.user_id = user_id
+    # def __init__(self, name, description, user_id):
+    #     self.name = name
+    #     self.description = description
+    #     self.user_id = user_id
 
     def __repr__(self):
-        return '{' + f' "id" : "{self.id}",' \
-                     f' "name" : "{self.name}",' \
-                     f' "description" : "{self.description}",' \
-                     f' "user_id" : "{self.user_id}",' \
-                     f' "email" : "{self.email}" ' + '}'
+        return str(CategorySchema().dump(self))
+        # return '{' + f' "id" : "{self.id}",' \
+        #              f' "name" : "{self.name}",' \
+        #              f' "description" : "{self.description}",' \
+        #              f' "user_id" : "{self.user_id}",' \
+        #              f' "email" : "{self.email}" ' + '}'
+
+    def info(self):
+        return CategorySchema().dump(self)
+
+
+class CategorySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = CategoryModel
+        # include_relationships = True
+        include_fk = True
+        load_instance = True
--- category_crud.py
+++ category_crud.py
@@ -2,7 +2,8 @@ from flask import request, redirect, url_for
 
 
 from flask_json import FlaskJSON
-from Models.category import CategoryModel
+
+from Models.category import CategoryModel, CategorySchema
 import login_module
 
 
@@ -19,52 +20,50 @@ def load_category_crud(application, database):
         if content_type == 'application/json':
             json_data = request.get_json()
             # print(json_data)
-            if \
-                    "name" not in json_data \
-                            or not json_data["name"] \
-                            or "description" not in json_data \
-                            or not json_data["description"] \
-                            or "user_id" not in json_data \
-                            or not json_data["user_id"]:
+            errors = CategorySchema().validate(data=json_data, session=db.session)
+            if errors or json_data["user_id"] != login_module.current_user.id:
+                print(errors)
                 return {
-                    "Response": "Missing information"
+                    "Response": "Missing or incorrect information"
                 }
-            existing_user_category = db.session.query.filter_by(
+            existing_user_category = db.session.query(CategoryModel).filter_by(
                     name=json_data['name'],
                     user_id=login_module.current_user.id).first()
             if existing_user_category:
                 return redirect(url_for(f'/category/"{existing_user_category.id}"', method='GET'))
             else:
-                new_category = CategoryModel(
-                    name=json_data["name"],
-                    description=json_data["description"],
-                    user_id=json_data["user_id"])
+                new_category = CategorySchema().load(data=json_data, session=db.session)
                 db.session.add(new_category)
                 db.session.commit()
-            return {
-                "Response": "Added successfully"
-            }
+                return {
+                    "Response": "Added successfully"
+                }
         else:
             return 'Content-Type not supported!'
 
     # retrieve all categories for current user
-    @app.route('/all_categories', methods='GET')  # Retrieve all categories
+    @app.route('/all_categories', methods=['GET'])  # Retrieve all categories
     # for test purposes disabled auth
-    # @login_module.login_required
+    @login_module.login_required
     def retrieve_multiple_categories():
-        category_id = request.args.get('id', type=int)
         categories = db.session.query(CategoryModel).filter_by(
-            user_id=login_module.current_user.id,
-            id=category_id).all()
+            user_id=login_module.current_user.id
+        ).all()
+        print(categories)
         if categories:
-            return str(categories)
+            result = {}
+            result['categories']=[]
+            for cat in categories:
+                result['categories'].append(CategoryModel.info(cat))
+            print(result)
+            return result  # str(categories)
         else:
             return "Category not found", 400
 
-    @app.route('/category/<id:int>', methods=['GET', 'PUT', 'DELETE'])  # RUD category
+    @app.route('/category/<category_id>', methods=['GET', 'PUT', 'DELETE'])  # RUD category
     @login_module.login_required
-    def rud_category():
-        request_cat_id = request.args.get('id', type=int)
+    def rud_category(category_id):
+        request_cat_id = category_id
         category = db.session.query(CategoryModel).filter_by(
             user_id=login_module.current_user.id,
             id=request_cat_id).first()
@@ -72,15 +71,25 @@ def load_category_crud(application, database):
 
         if category:
             if request.method == 'GET':
-                return str(category), 200
+                return CategoryModel.info(category), 200
             elif request.method == 'DELETE':
                 db.session.delete(category)
-                return 200
+                db.session.commit()
+                return 'ok', 200
             elif request.method == 'PUT':
                 if content_type == 'application/json':
                     json_data = request.get_json()
-                    category.name = json_data['name']
-                    category.description = json_data['description']
+
+                    errors = CategorySchema().validate(data=json_data,session= db)
+                    if errors:
+                        return {
+                            "Response": "Missing or incorrect information"
+                        }
+
+                    db.session.query(CategoryModel).filter_by(
+                        user_id=login_module.current_user.id,
+                        id=request_cat_id).update(json_data)
+
                     db.session.commit()
                     return "Ok", 200
                 else:
