3b55d7e45ddbf3bb312add35ccd700f70cc5f954
A <andr25@email.ua>
2022-11-22 22:46:33 +0200

    Add get by date endpoint and migration function

--- task_crud.py
+++ task_crud.py
@@ -1,10 +1,20 @@
 from flask_json import FlaskJSON
-import login_module
+from CRUDs import login_module
 from Models.tasks import TaskModel, TaskSchema
 from flask import request, jsonify
 from resp_error import errorss
 from marshmallow import ValidationError
 from flask import request, redirect, url_for
+from datetime import datetime, timedelta
+from sqlalchemy import and_, update
+
+
+# send func to worker thread
+def migrate_tasks(db):
+    tasks = db.session.query(TaskModel).filter_by().all()
+    for t in tasks:
+        t.deadline += timedelta(days=1)
+    db.session.commit()
 
 
 def load_task_crud(application, database):
@@ -12,7 +22,6 @@ def load_task_crud(application, database):
     db = database
 
     FlaskJSON(app)
-
 
     @app.route('/task', methods=['POST'])
     @login_module.login_required
@@ -23,6 +32,7 @@ def load_task_crud(application, database):
             if not json_data:
                 return errorss.bad_request
             json_data["user_id"] = login_module.current_user.id
+
             errors = TaskSchema().validate(data=json_data, session=db.session)
             if errors:
                 print(errors)
@@ -73,3 +83,32 @@ def load_task_crud(application, database):
             db.session.delete(task)
             db.session.commit()
             return {"message": "Deleted successfully"}, 200
+
+    @app.route('/task/date', methods=['GET'])  # Get list of all events for user from particular date to another
+    @login_module.login_required
+    def GetAllTasksForDate():
+        content_type = request.headers.get('Content-Type')
+        json_data = request.get_json()
+
+        if content_type != 'application/json':
+            return {"Response": "Wrong content type supplied, JSON expected"}, 400
+
+        if 'target_date' not in json_data or not json_data['target_date']:
+            return {"Response": "Missing or incorrect information"}, 400
+        json_data["target_date"] = str((json_data["target_date"])[2:])
+        target_date = datetime.strptime(json_data["target_date"], '%y-%m-%dT%H:%M:%S'),  # example "20/01/01 12:12:12"
+        target_date = target_date[0]
+
+        task_lst = db.session.query(TaskModel).filter_by(user_id=login_module.current_user.id) \
+            .filter(and_(TaskModel.deadline >= target_date,
+                         TaskModel.deadline < (target_date + timedelta(days=1)))
+                    ).all()
+
+        if not task_lst:
+            return {"Response": "Tasks not found"}, 400
+        tasks = []
+
+        for el in task_lst:
+            tasks.append(el.info())
+        # print(tasks)
+        return jsonify(tasks), 200
