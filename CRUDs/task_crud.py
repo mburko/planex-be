4c8176a98a52f119d6bb38901d87236b6458ca60
A <andr25@email.ua>
2022-12-19 15:32:07 +0200

    Get tasks by date

--- CRUDs/task_crud.py
+++ CRUDs/task_crud.py
@@ -109,7 +109,7 @@ def load_task_crud(application, database):
             db.session.commit()
             return {"message": "Deleted successfully"}, 200
 
-    @app.route('/task/date', methods=['GET'])  # Get list of all tasks for user for particular date
+    @app.route('/task/date', methods=['POST'])  # Get list of all tasks for user for particular date
     @login_module.login_required
     def GetAllTasksForDate():
         content_type = request.headers.get('Content-Type')
@@ -122,8 +122,8 @@ def load_task_crud(application, database):
             return {"Response": "Missing or incorrect information"}, 400
         json_data["target_date"] = str((json_data["target_date"])[2:])
         target_date = datetime.strptime(json_data["target_date"],
-                                        '%y-%m-%dT%H:%M:%S'),  # request example { "target_date":"2022-11-23T00:00:00"}
-        target_date = target_date[0]
+                                        '%y-%m-%dT%H:%M:%S')  # request example { "target_date":"2022-11-23T00:00:00"}
+        # target_date = target_date[0]
 
         task_lst = db.session.query(TaskModel).filter_by(user_id=login_module.current_user.id) \
             .filter(and_(TaskModel.deadline >= target_date,
